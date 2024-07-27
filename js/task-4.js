acc9fe4b27da7d3425e014c5cba6674c7c2b946a
Anita Hrabia <hrabia.anitagabriela@gmail.com>
2024-07-27 12:40:51 +0200

    task 1 added

---
+++ js/task-4.js
@@ -0,0 +1,7 @@
+'use strict';
+const getTotalBalanceByGender = (users, gender) => {
+  //przyjmuje dwa parametry:
+  //pierwszy parametr users — tablica obiektów użytkowników,
+  //drugi parametr gender — ciąg znaków przechowujący płeć.
+  //Funkcja powinna używać łańcucha wywołań metod i zwracać saldo użytkowników (właściwość balance), których płeć (właściwość gender) odpowiada wartości parametru gender.
+};
